14da8cf4bdadfead59ec45e52705b4423179f465
Reese <rkc8167@nyu.edu>
2025-12-03 18:04:16 +0800

    server is working

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const http = require('http');
 const express = require('express');
 const app = express();
-const server = app.listen(3000);
+const server = http.createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static('the-puppet-connection'));
+app.use('/', express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+  console.log('User connected:', socket.id);
 
-    socket.on('draw', (data) => {
-        socket.broadcast.emit('remote-draw', data);
-    });
+  socket.on('draw', (data) => {
+    console.log('Drawing data received:', data);
+    socket.broadcast.emit('remote-draw', data);
+  });
+
+  socket.on('redraw', (data) => {
+    console.log('Redraw:', data);
+    socket.broadcast.emit('remote-redraw', data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
